babcc16cfd3644139d9e9f24b0190dc0cb239923
Swapnil <swapniljoshiind@gmail.com>
2025-05-20 12:36:47 +0530

    1st commit 20/05

---
+++ git_testing/gitdata.py
@@ -0,0 +1,25 @@
+from urllib.response import addbase
+
+git --version           use for check git version.
+
+git config --list       to know configration in to the git
+git config global user.name "<USERNAME>"
+git config global user.email "<eamilid>"
+git init     track created folder through it
+git status   to chack the git repo status
+
+git add <file name>
+git add *             move all file and folder from the repo
+git add -A            move all file/ folder from the repo
+gir add .             move all hide file/ folder from the repo
+
+git restore --staged * move undo all file and folder from the stage to rapo
+git restore --staged . move undo all hide file/ folder from the stage to rapo
+
+
+
+git commit -m "comment"                move file and folder from staged to main secton
+
+
+
+
